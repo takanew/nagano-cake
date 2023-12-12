f04081b6171d187b6d39a75d4306f6b6f2a09115
takanew <145203231+takanew@users.noreply.github.com>
2023-12-12 11:07:53 +0000

    12.12一応保存

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+Admin.create!(
+  email: 'naganocake@gmail.com',## 任意のメールアドレス,
+  password: '123456',# 任意のパスワード
+)# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
