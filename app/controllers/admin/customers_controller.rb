f39e2f046f27a0f4b1ca29bf4177f3adbbdbe024
takanew <145203231+takanew@users.noreply.github.com>
2023-12-27 06:37:29 +0000

    admin会員詳細

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,4 +4,8 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
