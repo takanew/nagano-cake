7b10a1cc53e553a56885b139c567d63af9f49514
takanew <145203231+takanew@users.noreply.github.com>
2024-01-12 05:16:20 +0000

    注文情報入力画面作成（レイアウト抜き）

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,9 +2,17 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
   
   def new
+    @customer = current_customer
+    @order = Order.new
   end
   
   def confirm
+    @order = Order.new(order_params)
+    if @order.valid?
+      redirect_to confirm_order_path
+    else
+      render :new
+    end
   end
   
   def success
@@ -17,6 +25,12 @@ class Public::OrdersController < ApplicationController
   end
   
   def show
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ApplicationRecord
+   
+  belongs_to :customer
   
   enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  def full_address
+    '〒' + postal_code + ' ' + address + ' ' + first_name + ' ' + last_name
+  end
   
 end
