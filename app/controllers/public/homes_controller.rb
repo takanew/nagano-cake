c0e07d4fddb9d32cc5ee3660f41f5c03a26fcc83
takanew <145203231+takanew@users.noreply.github.com>
2024-01-24 04:56:30 +0000

    top.html.erb

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,10 @@ class Public::HomesController < ApplicationController
 
   # topページのアクション定義
   def top
+    @items = Item.order(id: :desc).limit(4)
   end
+  
+# Item.order(id: :desc): これは、Itemモデルのレコードをidの降順（大きいものから小さいものへ）で並べ替えます。ここで、orderはActiveRecordのメソッドで、データベースから取得したレコードの順序を指定します。:descは「降順」を意味します。
+# .limit(4): これは、上記の結果セットから最初の4つのレコードだけを取得します。limitもActiveRecordのメソッドで、取得するレコードの最大数を指定します。
 
 end
